2f0f7a44d046a69ed42816e34230c705dae671b3
Tempos <serge.kutwicki@gmail.com>
2018-12-31 19:33:10 +0200

    linted

--- finding_lanes/lanes.py
+++ finding_lanes/lanes.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
-    #print(image.shape)
+    # print(image.shape)
     y1 = image.shape[0]
-    y2 = int(y1*(3/5))
-    x1 = int((y1 - intercept)/slope)    # x = (y -b)/m
-    x2 = int((y2 - intercept)/slope)
+    y2 = int(y1 * (3 / 5))
+    x1 = int((y1 - intercept) / slope)    # x = (y - b) / m
+    x2 = int((y2 - intercept) / slope)
     return np.array([x1, y1, x2, y2])
 
 
@@ -19,7 +19,7 @@ def average_slope_intercept(image, lines):
     for line in lines:
         x1, y1, x2, y2 = line.reshape(4)
         parameters = np.polyfit((x1, x2), (y1, y2), 1)
-        #print(parameters)
+        # print(parameters)
         slope = parameters[0]
         intercept = parameters[1]
         if slope < 0:
@@ -53,8 +53,8 @@ def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
         for x1, y1, x2, y2 in lines:
-            #print(line)
-            #x1, y1, x2, y2 = line.reshape(4)
+            # print(line)
+            # x1, y1, x2, y2 = line.reshape(4)
             cv2.line(line_image, (x1, y1), (x2, y2),    # Draw lines on black canvas
                      (255, 0, 0),                       # Blue color
                      10)                                # Thickness of the line
@@ -63,9 +63,7 @@ def display_lines(image, lines):
 
 def region_of_interest(image):
     height = image.shape[0]
-    polygons = np.array([
-            [(200, height), (1100, height), (550, 250)]
-            ])
+    polygons = np.array([[(200, height), (1100, height), (550, 250)]])
     mask = np.zeros_like(image)         # Black image
     cv2.fillPoly(mask, polygons, 255)   # Fill mask with white triangle
     masked_image = cv2.bitwise_and(image, mask)     # Compare pixels
@@ -97,22 +95,22 @@ def region_of_interest(image):
 cap = cv2.VideoCapture("test2.mp4")
 while cap.isOpened():
     _, frame = cap.read()
-    canny_image = canny(frame)  # Sketch picture
+    canny_image = canny(frame)									# Sketch picture
     cropped_image = region_of_interest(canny_image)
 
     lines = cv2.HoughLinesP(cropped_image,
-                            2,  # Distance resolution in pxs
-                            np.pi / 180,  # Angle resolution in radians (1 degree = Pi/180)
-                            100,  # Min number of intersections
-                            np.array([ ]),  # placeholder array
-                            minLineLength=40,  # Length in pxs that we'll accept to the output
-                            maxLineGap=5)  # Distance between lines that we connect in one
+                            2,									# Distance resolution in pxs
+                            np.pi / 180,  						# Angle resolution in radians (1 degree = Pi/180)
+                            100,								# Min number of intersections
+                            np.array([]),						# placeholder array
+                            minLineLength=40,					# Length in pxs that we'll accept to the output
+                            maxLineGap=5)						# Distance between lines that we connect in one
     averaged_lines = average_slope_intercept(frame, lines)
     line_image = display_lines(frame, averaged_lines)
 
     combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    cv2.imshow("result", combo_image)   # window name, picture
-    if cv2.waitKey(1) == ord('q'):      # if cv2.waitKey(1) & 0xFF == ord('q'):
+    cv2.imshow("result", combo_image) 							# window name, picture
+    if cv2.waitKey(1) == ord('q'):    							# if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
 cv2.destroyAllWindows()
